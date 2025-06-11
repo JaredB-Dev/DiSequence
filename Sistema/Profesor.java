a76aecf33b451a5b1d816471c63cb081f0fe2dc5
BolivarAlvarado <bolivaralvarado09@gmail.com>
2025-06-11 16:13:16 -0500

    codigo

--- Sistema/Profesor.java
+++ Sistema/Profesor.java
@@ -5,6 +5,6 @@ public class Profesor extends PersonalAcademico {
     private List<Curso> cursosACargo;
     public void cargarActividadSumativa(Curso curso, ActividadSumativa actividad) {}
     public void calificarActividadSumativa(Curso curso, Estudiante estudiante, ActividadSumativa actividad) {}
-    public void aceptarInscripcion(Curso curso, Estudiante estudiante) {}
+    public void aceptarInscripcion(Curso curso, String usuarioEstudiante) {}
     public void crearNuevoForo(Foro foro) {}
 }
