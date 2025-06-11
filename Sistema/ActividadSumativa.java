8876821b8642a15904fe8e4eaade7c624d380006
BolivarAlvarado <bolivaralvarado09@gmail.com>
2025-06-11 16:18:17 -0500

    codigo

--- Sistema/ActividadSumativa.java
+++ Sistema/ActividadSumativa.java
@@ -2,10 +2,10 @@ package Sistema;
 import java.util.Date;
 // clase actividad sumativa
 public class ActividadSumativa {
-    private String id;
-    private String titulo;
-    private Date fechaDeEntrega;
-    private float puntajeMaximo;
-    private String contenido;
-    private float calificacion;
+    protected String id;
+    protected String titulo;
+    protected Date fechaDeEntrega;
+    protected float puntajeMaximo;
+    protected String contenido;
+    protected float calificacion;
 }
